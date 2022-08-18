a82a2549aafdf0ef7de617dfdff0b02e0b18248e
aniket-6234 <aniketashok1999@gmail.com>
2022-08-18 11:07:16 +0530

    SERVER

---
+++ backend/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
+require("dotenv").config();
+
+// initiliaze express app
+const app = express();
+const port = process.env.PORT || 5000; // port number
+
+// middlerware
+app.use(cors());
+
+// creating server
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`)
+  })
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  })
+
+  socket.on("disconnect", () => {
+    console.log(`User Disconnected: ${socket.id}`);
+  })
+})
+
+// listening on port
+server.listen(port, () => {
+  console.log(`Listening on Port ${port}`);
+});
